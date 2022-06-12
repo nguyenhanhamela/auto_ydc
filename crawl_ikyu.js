b2dddf0ebef6655f86a0aeae89e562bf89662d12
Hanh Nguyen <hanhnguyen@Hanhs-MacBook-Pro.local>
2022-06-12 11:50:09 +0700

    Initilize Project

---
+++ crawl_ikyu.js
@@ -0,0 +1,166 @@
+require("chromedriver");
+
+const moment = require("moment");
+
+let swd = require("selenium-webdriver");
+let browser = new swd.Builder();
+let driver = browser.forBrowser("chrome").build();
+
+let { facality_id, operator_id, pass_ikyu } = require("./user.json");
+
+// Step 1 - Opening web page
+let driverToOpen = driver.get(
+	"https://www.ikyu.com/accommodation/ap/AsfW10101.aspx?AccommodationId=00000440"
+);
+const handleDateMonth = function () {
+	// Get moment at start date of previous month
+	var prevMonth = moment().subtract(1, "month").startOf("month");
+	var prevMonthDays = prevMonth.daysInMonth();
+
+	// Array to collect dates of previous month
+	var prevMonthDates = [];
+
+	for (var i = 0; i < prevMonthDays; i++) {
+		// Calculate moment based on start of previous month, plus day offset
+		var prevMonthDay = prevMonth.clone().add(i, "days").format("YYYY/MM/DD");
+
+		prevMonthDates.push(prevMonthDay);
+	}
+	var searchStartDate = prevMonthDates[0];
+	var searchEndDate = prevMonthDates[prevMonthDates.length - 1];
+
+	// Get date format: from 2022年05月 to 2022年05月
+	var rankStartDate = prevMonthDates[0].slice(0,7).replace(/\//g, '年').concat('月');
+	var rankEndDate = prevMonthDates[prevMonthDates.length - 1].slice(0,7).replace(/\//g, '年').concat('月');
+	var dateRank = prevMonthDates[0].slice(0,7).replace(/\//g, '');
+	const lstDate = [];
+	lstDate.push(searchStartDate,searchEndDate, rankStartDate, rankEndDate, dateRank)
+	return lstDate;
+}
+driverToOpen
+	.then(async () => {
+		let findTimeOutP = driver.manage().setTimeouts({
+			implicit: 10000, // 10 seconds
+		});
+		driver.manage().window().maximize();
+		return findTimeOutP;
+	})
+	.then(async () => {
+		let facilityID = driver
+			.findElement(
+				swd.By.xpath(
+					"//input[@id='ctl00_ContentPlaceHolderMain_TriesteTextAccommodationID']"
+				)
+			)
+			.sendKeys(facality_id);
+		return facilityID;
+	})
+	.then(async () => {
+		let operatorID = driver
+			.findElement(
+				swd.By.xpath(
+					"//input[@id='ctl00_ContentPlaceHolderMain_TriesteTextOperatorID']"
+				)
+			)
+			.sendKeys(operator_id);
+		return operatorID;
+	})
+	.then(async () => {
+		let password = driver
+			.findElement(
+				swd.By.xpath(
+					"//input[@id='ctl00_ContentPlaceHolderMain_TriesteTextPassword']"
+				)
+			)
+			.sendKeys(pass_ikyu);
+		return password;
+	})
+	.then(async () => {
+		let btnSignIn = driver
+			.findElement(
+				swd.By.xpath(
+					"//input[@name='ctl00$ContentPlaceHolderMain$TriesteButtonLogin']"
+				)
+			)
+			.click();
+		return btnSignIn;
+	})
+	.then(async () => {
+		driver.get(
+			"https://www.ikyu.com/accommodation/ap/rsrv/AsfW60101.aspx?AccommodationId=00000440&AsfMenuId=ASF_14"
+		);
+	})
+	.then(async () => {
+		var getDate = handleDateMonth();
+		// console.log(getDate);
+		await driver.findElement(swd.By.xpath("//input[@id='ctl00_ContentPlaceHolderMain_TriesteRadioDaySelect_2']")).click();
+		await driver.findElement(swd.By.xpath("//input[@name='ctl00$ContentPlaceHolderMain$TriesteTextDayFrom']"))
+			.clear();
+		await driver.findElement(swd.By.xpath("//input[@name='ctl00$ContentPlaceHolderMain$TriesteTextDayFrom']"))
+			.sendKeys(getDate[0])
+
+		await driver.findElement(swd.By.xpath("//input[@name='ctl00$ContentPlaceHolderMain$TriesteTextDayTo']"))
+			.clear();
+		await driver.findElement(swd.By.xpath("//input[@name='ctl00$ContentPlaceHolderMain$TriesteTextDayTo']"))
+			.sendKeys(getDate[1])
+	})
+	.then(async () => {
+		let calcellationText = driver.findElement(swd.By.xpath("//label[@for='ctl00_ContentPlaceHolderMain_TriesteCheckStatus']"));
+		driver.sleep(30)
+		let scroll = await driver.executeScript("arguments[0].scrollIntoView();", calcellationText);
+		return scroll;
+	})
+	.then(async () => {
+		await driver.findElement(swd.By.xpath("//input[@id='ctl00_ContentPlaceHolderMain_TriesteCheckStatus']")).click();
+		driver.sleep(10)
+		await driver.findElement(swd.By.xpath("//input[@id='ctl00_ContentPlaceHolderMain_TriesteButtonSearch']")).click();
+		driver.sleep(30)
+	})
+	.then(async () => {
+		/* ikyu data
+		{
+			"date": "202205",
+			"excluding_cancellation_revenue": "14613091",
+			"prefecture_sale_rank": "8/150",
+			"region_sale_rank": "2/52"
+		}*/
+		driver.sleep(10);
+		var ikyu_data = {}
+		let cancellation_revenue = await driver.findElement(swd.By.xpath("//span[@id='ctl00_ContentPlaceHolderMain_TriesteLabelBookingAmount']")).getText();
+		let revenue = cancellation_revenue.replace(/,/g, "").replace(/\\/g, '')
+		ikyu_data["excluding_cancellation_revenue"] = revenue
+		console.log(ikyu_data);
+		return ikyu_data;
+	})
+	.then( async (ikyu_data) => {
+		var getDate = handleDateMonth();
+		driver.get('https://www.ikyu.com/accommodation/ap/rsrv/AsfW70301.aspx?AccommodationId=00000440&AsfMenuId=ASF_24');
+		await driver.findElement(swd.By.id('ctl00_ContentPlaceHolderMain_TargetYMFrom')).sendKeys(getDate[2]);
+		await driver.findElement(swd.By.id('ctl00_ContentPlaceHolderMain_TargetYMTo')).sendKeys(getDate[3]);
+		await driver.findElement(swd.By.id('ctl00_ContentPlaceHolderMain_TriesteButtonSearch')).click();
+		await driver.sleep(15)
+		ikyu_data["date"] = getDate[4];
+		return ikyu_data;
+	})
+	.then( async (ikyu_data) => {
+		let lstRows = await driver.findElements(
+			swd.By.xpath("//div[@id='ctl00_ContentPlaceHolderMain_panelSalesResultRankingList']//table//tr")
+		);
+		var dataCol = [];
+		let lstCols = await driver.findElements(swd.By.xpath("//div[@id='ctl00_ContentPlaceHolderMain_panelSalesResultRankingList']//table/tbody/tr[3]/td"));
+		dataCol = [];
+		for (col of lstCols) {
+			let val = await col.getText();
+			dataCol.push(val);
+		}
+		console.log(dataCol);
+		ikyu_data["prefecture_sale_rank"] = String(dataCol[3])
+		ikyu_data["region_sale_rank"] = String(dataCol[4])
+		console.log(ikyu_data);
+	})
+	.then (async() => {
+		driver.quit();
+	})
+	.catch(function (err) {
+		console.log("Error ", err, "Have some error");
+	});
---
+++ crawl_rakuten.js
@@ -0,0 +1,169 @@
+require("chromedriver");
+
+let swd = require("selenium-webdriver");
+let browser = new swd.Builder();
+let tab = browser.forBrowser("chrome").build();
+
+let { ID, password } = require("./user.json");
+
+// Step 1 - Opening web page
+let tabToOpen = tab.get("https://manage.travel.rakuten.co.jp/");
+tabToOpen
+	.then(function () {
+		let findTimeOutP = tab.manage().setTimeouts({
+			implicit: 10000, // 10 seconds
+		});
+		return findTimeOutP;
+	})
+	.then(function () {
+		let promiseUsernameBox = tab.findElement(
+			swd.By.xpath("//input[@name='f_id']")
+		);
+		return promiseUsernameBox;
+	})
+	.then(function (usernameBox) {
+		let promiseFillUsername = usernameBox.sendKeys(ID);
+		return promiseFillUsername;
+	})
+	.then(function () {
+		let promisePasswordBox = tab.findElement(
+			swd.By.xpath("//input[@name='f_pass']")
+		);
+		return promisePasswordBox;
+	})
+	.then(function (passwordBox) {
+		let promiseFillPassword = passwordBox.sendKeys(password);
+		return promiseFillPassword;
+	})
+	.then(function () {
+		let promiseSignInBtn = tab.findElement(
+			swd.By.xpath("//input[@value='ログイン']")
+		);
+		return promiseSignInBtn;
+	})
+	.then(function (signInBtn) {
+		let promiseClickSignIn = signInBtn.click();
+		return promiseClickSignIn;
+	})
+	.then(function () {
+		let promiseGetBtn = tab.findElement(
+			swd.By.xpath("//input[@value='宿泊施設カルテ']")
+		);
+		return promiseGetBtn;
+	})
+	.then(function (promiseGetBtn) {
+		let promiseClickData = promiseGetBtn.click();
+		return promiseClickData;
+	})
+	.then(function () {
+		var lstWindows = tab.getAllWindowHandles();
+		return lstWindows;
+	})
+	.then(function (lstWindows) {
+		// console.log(lstWindows) // print all list of windows
+		// var getCurrentWindow = tab.getWindowHandle();
+		// for (let i in lstWindows) {
+		//     if (i != getCurrentWindow) {
+		//         var st = tab.switchTo().window(i)
+		//     }
+		// }
+		var st = tab.switchTo().window(lstWindows[1]);
+		return st;
+	})
+	.then(function (st) {
+		// console.log(st)
+		var getTab = tab.getCurrentUrl();
+		return getTab;
+	})
+	.then(function (getTab) {
+		// console.log(getTab)
+		let promiseDisplayBtn = tab.findElement(
+			swd.By.xpath("//input[@value='表示']")
+		);
+		return promiseDisplayBtn;
+	})
+	.then(function (promiseDisplayBtn) {
+		let promiseClickDisplay = promiseDisplayBtn.click();
+		tab.sleep(10);
+		return promiseClickDisplay;
+	})
+	.then(async () => {
+		// json sample:
+		/*
+			{
+				"colum A:" "row 1, column 1",
+				"colum B:" "row 1, column 2",
+			}
+			*/
+		var tempArray = [];
+		const table = [];
+		for (let k = 1; k < 14; k++) {
+			const name = await tab
+				.findElement(
+					swd.By.xpath('//table[@id="table1"]/thead/tr[3]/td[' + k + "]")
+				)
+				.getText();
+			tempArray.push(name);
+		}
+		table.push({
+			date: tempArray[0].replace(/\//g, ""),
+			pv: tempArray[9].replace(/,/g, ""),
+			access_amount: tempArray[3].replace(/,/g, ""),
+			conversion_rate: tempArray[5],
+			avg_pv: tempArray[10].replace(/,/g, ""),
+			access_avg_amount: tempArray[4].replace(/,/g, ""),
+			avg_conversion_rate: tempArray[6],
+			reservation_avg_unit_price:
+				String(Math.round(tempArray[1].replace(/,/g, "") / tempArray[2].replace(/,/g, ""))),
+			reservation_avg_amount:
+				String(Math.round((parseInt(tempArray[4].replace(/,/g, "")) *
+					parseInt(tempArray[6].replace(/%/g, ""))) /
+					100)),
+			reservation_avg_revenue:
+				String(Math.round(tempArray[8].replace(/,/g, "") *
+					tempArray[4].replace(/,/g, "") *
+					(parseInt(tempArray[6].replace(/%/g, "")) / 100)))
+		});
+		return table;
+	})
+	.then(async (table) => {
+		// Scroll to table 4
+		let element = tab.findElement(swd.By.id("table4"));
+		tab.executeScript("arguments[0].scrollIntoView(true);", element);
+
+		// Get monthly data
+		var daily_monthly_reports = [], reports = []
+		let lstRows = await tab.findElements(
+			swd.By.xpath('//table[@id="table4"]/thead/tr')
+		);
+		var dataCol = [];
+		// loop rows
+		for (let row = 3; row < lstRows.length; row++) {
+			let lstCols = await lstRows[row].findElements(swd.By.css("td"));
+			dataCol[row] = [];
+			for (col of lstCols) {
+				let val = await col.getText();
+				dataCol[row].push(val);
+			}
+		}
+		dataCol.map((data) => {
+			reports.push({
+				date: data[0],
+				pv: data[6],
+				reservation_avg_unit_price: data[5].replace(/,/g, ""),
+				reservation_avg_amount: String(Math.round(data[3] * data[4].replace(/%/g, "") / 100)),
+				reservation_avg_revenue: String(Math.round(data[5].replace(/,/g, "") * data[3] * data[4].replace(/%/g, "") / 100))
+			});
+		});
+		// console.log(reports);
+		table[0].daily_monthly_reports = reports
+		return table;
+	})
+	.then(async (table) => {
+		console.dir(table, { depth: null, colors: true })
+		tab.quit();
+	})
+
+	.catch(function (err) {
+		console.log("Error ", err, "Have some error");
+	});
